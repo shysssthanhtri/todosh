fab161d317152d63904c2b816b874397ac636bd5
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-03-05 18:24:17 +0700

    feat: descrease limit of todo title

--- schemas/todo.ts
+++ schemas/todo.ts
@@ -3,5 +3,5 @@ import z from "zod";
 import { TodoModel } from "@/models";
 
 export const TodoSchema = z.object(TodoModel.shape).extend({
-  title: z.string().min(5).max(50),
+  title: z.string().min(3).max(50),
 });
