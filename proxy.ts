f866e9a0d0f49d8fe205f2107ef2b2312445194a
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-03-06 23:20:08 +0700

    feat: fix facebook image

--- proxy.ts
+++ proxy.ts
@@ -2,5 +2,7 @@ export { auth as proxy } from "@/auth";
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|images|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|images|_next/image|favicon.ico|opengraph-image).*)",
+  ],
 };
