861d32e9b7dd947bb2fedd0bb95babf1c27750b4
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-03-06 22:50:35 +0700

    feat: improve seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
       { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
-    apple: [{ url: "/icons/icon-192x192.png" }],
+    apple: [{ url: "/icons/apple-touch-icon.png" }],
   },
 };
 
