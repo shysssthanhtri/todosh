66794019570d95c308044bfb6bddfcf4443d88e3
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-03-09 21:36:30 +0700

    fix: extend proxy configuration to exclude sitemap path from middleware invocation

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,6 @@ export { auth as proxy } from "@/auth";
 // Optionally, don't invoke Middleware on some paths
 export const config = {
   matcher: [
-    "/((?!api|_next/static|images|_next/image|favicon.ico|opengraph-image|google955bba4722ed128b).*)",
+    "/((?!api|_next/static|images|_next/image|favicon.ico|opengraph-image|google955bba4722ed128b|sitemap).*)",
   ],
 };
